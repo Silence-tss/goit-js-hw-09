5bd067f0538287e58a43146a18d352a52360bdb7
Silence-tss <99872463+Silence-tss@users.noreply.github.com>
2023-11-03 15:54:25 +0200

    first commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const bodySite = document.querySelector("body");
+let timerId = null;
+
+
+buttonStart.addEventListener("click", onClickButtonStart);
+
+function onClickButtonStart() {
+    // buttonStart.classList.toggle("active").disabled = true;
+    timerId = setInterval(() => {
+    bodySite.style.backgroundColor= getRandomHexColor()}, 1000);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+buttonStop.addEventListener("click", onClickButtonStop);
+
+function onClickButtonStop() {
+    clearInterval(timerId);
+}
+
