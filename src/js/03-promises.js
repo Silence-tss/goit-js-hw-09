5f72ad3c04fb7f35e8e117fe670db93fdd99bb90
Silence-tss <99872463+Silence-tss@users.noreply.github.com>
2023-12-04 18:05:06 +0200

    promise proba 1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+// import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const { delay, step, amount} = event.currentTarget.elements;
+
+  setTimeout (() => {
+    for (let i = 1; i <= +amount.value; i++) {
+      createPromise(i, (step.value * i))
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+    }
+  }, +delay.value)
+}
+
 function createPromise(position, delay) {
+return new Promise ((resolve, reject) => {
+setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    reject(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+}, delay)
+})
+
 }
+
